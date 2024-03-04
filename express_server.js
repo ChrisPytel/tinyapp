77f7d8f30b052dbc68b2fbd1718e0c6526b77a71
ChrisPytel <chris_pytel@hotmail.com>
2024-03-04 01:24:34 -0500

    add route for /urls

--- express_server.js
+++ express_server.js
@@ -2,10 +2,9 @@
 
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 3333;
 
 app.set("view engine", "ejs"); //Tells the Express app to use EJS as its templating engine.
-
 
 
 const urlDatabase = {
@@ -18,24 +17,21 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);  //express handles this response and returns a object in JSON format
 });
 
+
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>This is me saying <b>HELLO WORLD</b></body></html>\n"); //returns an HTML response
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello, welcome to the landing page for tinyApp!");
 });
 
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- }); //for testing variable continuity over get requests
-
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`express_server.js listening on port ${PORT}!`);
