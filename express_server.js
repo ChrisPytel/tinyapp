704a81dcf233f2d5f374b089efb761b4fc68abe6
ChrisPytel <chris_pytel@hotmail.com>
2024-03-05 01:45:57 -0500

    add generateRandomString to express_server and add POST routes

--- express_server.js
+++ express_server.js
@@ -4,8 +4,11 @@ const express = require("express");
 const app = express();
 const PORT = 3333;
 
-app.set("view engine", "ejs"); //Tells the Express app to use EJS as its templating engine.
+//Tells the Express app to use EJS as its templating engine.
+app.set("view engine", "ejs"); 
 
+//Tells the web server to understand and process information sent from web forms / POST calls
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,8 +20,6 @@ const urlDatabase = {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);  //express handles this response and returns a object in JSON format
 });
-
-
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>This is me saying <b>HELLO WORLD</b></body></html>\n"); //returns an HTML response
@@ -34,11 +35,34 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
-  res.send("this is the url id"); //returns an HTML response
+
+//must be above the .get method that checks for parameters ids
+app.get("/urls/new", (req, res) => { 
+  res.render("urls_new");
 });
 
+app.get("/urls/:id", (req, res) => {
+  res.send("this is the url id"); 
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`express_server.js listening on port ${PORT}!`);
 });
+
+
+// return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+const randomString = Array.from({ length: 6 }, () => Math.floor(Math.random() * 16).toString(16)).join('');
+
+function generateRandomString() {
+  // copied from gnerateUID from music library challenge
+  // const randomStringA = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);   
+  const randomStringB = (Math.random().toString(16).substring(2, 8));  //modified to return a 6 digits/letter string
+  console.log(randomStringB);
+  return randomStringB;
+};
